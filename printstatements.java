67c3cd3d696555b6e313d31968dcbaed7289e238
Rubz <grewalrubal97@gmail.com>
2024-03-11 13:42:54 -0600

    project SNDL first commit

--- printstatements.java
+++ printstatements.java
@@ -4,7 +4,9 @@ public class printstatements {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("index");
+		System.out.print("index1");
+		System.out.print("index2");
+		System.out.print("index3");
 	}
 
 }
